de23e15e0e8a1af85be188246e9c7093e6420210
Mariana Marques <ma_oliveiramarques@hotmail.com>
2022-10-20 23:29:26 +0100

    Aula 2 iMask

--- src/main.js
+++ src/main.js
@@ -26,3 +26,21 @@ const securityCodePattern = {
   mask: "000"
 }
 const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector('#expiration-date')
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    }
+
+  }
+}
