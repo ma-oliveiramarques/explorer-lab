7a9ef1027a316c378ad29079e96f344abb90bfb1
Mariana Marques <ma_oliveiramarques@hotmail.com>
2022-10-29 23:12:12 +0100

    project finished

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
-
 function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
@@ -76,3 +75,58 @@ const cardNumberPattern = {
 
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
 
+// we use # because its an id
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado!");
+})
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  // we are selecting a class, so we use the . and inside the class cc-holder, we want to access the class value
+  const ccHolder = document.querySelector(".cc-holder .value")
+  // if there's no input, we keep the name "Fulano de Silva"
+  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+
+// we are going to wait to get the information written by the user at the Security Code section
+securityCodeMasked.on("accept", () => {
+  // it will return the value of the user's input
+  updateSecurityCode(securityCodeMasked.value);
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value")
+  // if there's no input, we keep the "123" code
+  ccSecurity.innerText = code.length === 0 ? "123" : code
+}
+
+cardNumberMasked.on("accept", () => {
+  // we are accessing the information of the mask corresponding the cardtype, accordingly to the card number
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+
+  setCardType(cardType)
+  // it will return the value of the user's input
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+  // if there's no input, we keep the default card number
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+expirationDateMasked.on("accept", () => {
+  // it will return the value of the user's input
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value")
+  // if there's no input, we keep the default date
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
